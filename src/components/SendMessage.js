960eebcb9e4095d3d67eef7ba06a7d2df779f2eb
mbawon <jlucky046@gmail.com>
2022-08-10 09:46:26 +0000

    Added send message component and updated index.css file with component styles

---
+++ src/components/SendMessage.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Input, Button } from 'antd'
+import { SendOutlined } from '@ant-design/icons'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage } from '../slice/message'
+import { selectUser } from '../slice/auth'
+
+export const SendMessage = () => {
+    const dispatch = useDispatch()
+    const [message, setMessage] = useState("")
+
+    const user = useSelector(selectUser) //get all users from store
+
+    const sendMessage = () => {
+        const userMessage = {} //initial an empty object
+
+        userMessage.messageId = new Date().getTime() //unique message id
+        userMessage.senderId = user.userId //sender's user id from user list
+        userMessage.senderName = user.username //sender's username from user list
+        userMessage.message = message //message of the user
+
+        dispatch(addMessage(userMessage)) //dispatch message to store.
+        setMessage("") //clear input
+    }
+
+    return (
+        <div className='send-message'>
+            <Input
+                size='large'
+                placeholder='Type here...'
+                prefix="Message:"
+                style={{
+                    height: "60px"
+                }}
+                onChange={(e) => setMessage(e.target.value)}
+                onPressEnter={sendMessage}
+                value={message}
+            />
+            <Button onClick={sendMessage} type="primary" size='large' icon={<SendOutlined />} style={{ height: 60, width: 70 }}></Button>
+
+        </div>
+    )
+}
