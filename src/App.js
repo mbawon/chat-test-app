fa5eb00a62913e4db161f4222809b0083f3e8990
mbawon <jlucky046@gmail.com>
2022-08-10 10:50:55 +0000

    update app.js file to bring all components together and to make sure they are accessible only after a successful login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { ChatPage } from './containers/ChatPage';
+import { Login } from './containers/Login';
+import { selectUser } from './slice/auth';
 
 export const App = () => {
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const user = useSelector(selectUser)
+
+  useEffect(() => {
+    setLoggedIn(Object.keys(user).length > 0)
+  }, [user])
+  
   return (
-    <div>Chat</div>
-  )
+    <div>
+      {loggedIn ? <ChatPage /> : <Login />}
+    </div>
+  );
 }
