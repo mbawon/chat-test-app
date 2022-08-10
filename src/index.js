c82f87e45d9fad6c1935a5cb6b96ce9c68e095ad
mbawon <jlucky046@gmail.com>
2022-08-10 09:32:12 +0000

    Updated index.js file to wrap app with redux store andalso in this file import global styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css'
 import './index.css';
+import { Provider } from "react-redux";
+import store from './store.js';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import App from './App';
+
+let persistore = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistore}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
