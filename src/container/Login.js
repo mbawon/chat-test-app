36e80288e9c1e58235b2e57a43eef506d7f022f9
mbawon <jlucky046@gmail.com>
2022-08-10 10:47:03 +0000

    update login page with a functionality to further check there is an active user in chat room with same name one is trying to use

--- src/container/Login.js
+++ src/container/Login.js
@@ -7,6 +7,7 @@ import { addUser, selectUsers } from '../slice/user-slice'
 export const Login = () => {
     const dispatch = useDispatch()
     const [senderName, setSenderName] = useState("")
+    const [prompt, setPrompt] = useState("")
 
     const users = useSelector(selectUsers)
 
@@ -14,23 +15,36 @@ export const Login = () => {
 
         e.preventDefault();
 
-        const user = users.filter(user=>user.username.toLowerCase() === senderName.toLowerCase())
+        const user = users.filter(user=>user.username.toLowerCase() === senderName.toLowerCase()) //get user details
+        //check if user exist with the name provided
         if(user.length > 0){
-            dispatch(login(user[0]))
-        }else{
-            const newUser = {}
+            const isUserActive = users.filter(user=>user.username.toLowerCase() === senderName.toLowerCase()) //check if active user exist with this name
+            if(isUserActive.length === 0){
+                dispatch(login(user[0])) // add user to the list loggedin users
+            }else{
+                setTimeout(() => {
+                    setPrompt("")
+                }, 5000);
+                setPrompt("There is an active user in the chat room with this name.")
+            }
+        }else{ //else
+            const newUser = {} //initials an empty object and add user attributes
             newUser.userId = new Date().getTime()
             newUser.username = senderName
-            dispatch(addUser(newUser))
-            dispatch(login(newUser))
+            dispatch(addUser(newUser)) //create user
+            dispatch(login(newUser)) // add user to the list of loggedin users
         }
     }
 
     return (
         <div className="login">
             <form onSubmit={handleLogin}>
+                {prompt && <span>{prompt}</span>}
                 <Input size="large" onChange={(e) => setSenderName(e.target.value)} />
-                <Button onClick={handleLogin} type="primary" className="w-100">LOGIN</Button>
+                
+                <Button onClick={handleLogin} type="primary" style={{width:"100%", marginTop:15}}>
+                    LOGIN
+                </Button>
             </form>
         </div>
     )
