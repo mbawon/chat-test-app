f22b22109cf53a0efd8a7bbc8e94843c07b2ada1
mbawon <jlucky046@gmail.com>
2022-08-10 10:31:02 +0000

    Added a chat room page. this brings all components together

---
+++ src/container/ChatRoom.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from 'react'
+import { useState } from 'react'
+import { Spin } from 'antd'
+import { useSelector, useDispatch } from 'react-redux'
+import { ChatHeader } from '../components/ChatHeader'
+import { ChatMessage } from '../components/ChatMessage'
+import { SendMessage } from '../components/SendMessage'
+import { getMessages, selectPaginated } from '../slice/message'
+
+export const ChatRoom = () => {
+    const dispatch = useDispatch()
+    const dummy = useRef();
+    const paginatedMessages = useSelector(selectPaginated)
+
+    const [pageSize, setPageSize] = useState(25)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        dispatch(getMessages(pageSize))
+    }, [pageSize, dispatch])
+
+    useEffect(() => {
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+    }, [])
+
+    const handleScroll = (e) => {
+        if (e.currentTarget.scrollTop === 0) {
+            setLoading(true)
+            setTimeout(() => {
+                setLoading(false)
+                setPageSize(pageSize + (25))
+            }, 5000);
+        }
+    }
+
+    return (
+        <div className='chat-page'>
+            <ChatHeader />
+            <section onScroll={handleScroll}>
+                {loading &&
+                    <div className='spinner'>
+                        <Spin />
+                    </div>
+                }
+                {paginatedMessages && paginatedMessages.map(msg => <ChatMessage key={msg.messageId} senderId={msg.senderId} message={msg.message} />)}
+                <span ref={dummy}></span>
+            </section>
+            <SendMessage />
+        </div>
+    )
+}
